3b9477e4aaa6854f2e96627ef6c82fcffd339fa0
Alex Kovacs <alexdemaster@proton.me>
2023-11-18 08:18:48 +0200

    small QoL stuff

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import express from "express";
-import { google, youtube_v3 } from "googleapis";
+import { google, type youtube_v3 } from "googleapis";
 import inquirer from "inquirer";
+import { performance } from "node:perf_hooks";
 import open from "open";
 import json from "./config.json" assert { type: "json" };
 
 const { client_id, client_secret, redirect_url } = json;
+
+if (
+  client_id.length < 1 ||
+  client_secret.length < 1 ||
+  redirect_url.length < 1
+) {
+  console.error("Please fill in the config.json file.");
+  process.exit(1);
+}
+
 const oauth = new google.auth.OAuth2(client_id, client_secret, redirect_url);
 const scopes = [
   "https://www.googleapis.com/auth/youtube",
@@ -19,7 +30,7 @@ server.listen(3000, () => {
 
 server.get("/oauth2callback", async (req, res) => {
   const { code } = req.query;
-  if (!code) return console.log("login went wrong");
+  if (!code) return res.end("login went wrong, try again.");
 
   const { tokens } = await oauth.getToken(code as string);
   oauth.setCredentials(tokens);
@@ -71,6 +82,7 @@ async function replace(
     `Replacing the description for ${filtered.length} videos, this may take a while...`
   );
 
+  const start = performance.now();
   for (const video of filtered) {
     const categoryId = await yt.videos.list({
       part: ["snippet"],
@@ -79,7 +91,6 @@ async function replace(
 
     await yt.videos.update({
       part: ["id", "snippet"],
-
       requestBody: {
         id: video.id?.videoId,
         snippet: {
@@ -90,8 +101,9 @@ async function replace(
       },
     });
   }
+  const end = performance.now();
 
-  console.log("Great success!");
+  console.log(`Great success! Took: ${(end - start).toPrecision(2)}ms`);
   process.exit();
 }
 
